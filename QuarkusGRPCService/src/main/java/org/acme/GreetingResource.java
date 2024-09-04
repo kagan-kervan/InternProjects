76a155ec84107b42f2ab0d011f79d8383b30368b
kagan-kervan <ahmetkagankervan@gmail.com>
2024-09-04 15:33:50 +0300

    Created gRPC service and client implementation in Quarkus

--- QuarkusGRPCService/src/main/java/org/acme/GreetingResource.java
+++ QuarkusGRPCService/src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,50 @@
 package org.acme;
 
+import io.smallrye.mutiny.Uni;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
+import org.acme.protos.Greeter;
+import org.acme.protos.GreeterGrpc;
+import org.acme.protos.HelloReply;
+import org.acme.protos.HelloRequest;
+import org.jboss.resteasy.reactive.RestQuery;
 
 @Path("/hello")
 public class GreetingResource {
 
+    @Inject
+    GreetingsService service;
+
+    private final GreeterGrpc.GreeterBlockingStub blockingStub;
+
+    @Inject
+    public GreetingResource(GreeterGrpc.GreeterBlockingStub greeterBlockingStub){
+        this.blockingStub = greeterBlockingStub;
+    }
+    @GET
+    @Path("/grpc/{name}")
+    public String HelloFromGRPC(String name){
+        HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+        HelloReply reply = blockingStub.sayHello(request);
+        return reply.getMessage();
+    }
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
+    public String hello(@RestQuery("name") String name) {
+        return "Hello from Quarkus "+ name;
     }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    public String Greeting(String name){
+        return service.greeting(name);
+    }
+
 }
---
+++ QuarkusGRPCService/src/main/java/org/acme/HelloService.java
@@ -0,0 +1,21 @@
+package org.acme;
+
+import io.grpc.stub.StreamObserver;
+import io.quarkus.grpc.GrpcService;
+import jakarta.inject.Singleton;
+import org.acme.protos.GreeterGrpc;
+import org.acme.protos.HelloReply;
+import org.acme.protos.HelloRequest;
+
+@GrpcService
+public class HelloService extends GreeterGrpc.GreeterImplBase {
+
+
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        String name = request.getName();
+        String message = "Hello " + name;
+        responseObserver.onNext(HelloReply.newBuilder().setMessage(message).build());
+        responseObserver.onCompleted();
+    }
+}
